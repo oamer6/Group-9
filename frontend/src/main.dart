5dcd8b5f44c11b0cf68bd84792c94f09732704eb
ReaganChapman <46576527+ReaganChapman@users.noreply.github.com>
2020-07-14 14:27:54 -0400

    Add files via upload

--- frontend/src/main.dart
+++ frontend/src/main.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:morseapp/pages/home.dart';
 import 'package:morseapp/pages/translation.dart';
 import 'package:morseapp/pages/loading.dart';
+import 'package:morseapp/pages/login.dart';
+import 'package:morseapp/pages/signup.dart';
+import 'package:morseapp/pages/loading.dart';
+import 'package:morseapp/pages/forgot.dart';
+import 'package:morseapp/pages/inbox.dart';
+import 'package:morseapp/pages/send.dart';
+import 'package:morseapp/pages/key.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/home',
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
+    '/forgot': (context) => Forgot(),
+    '/signup': (context) => SignUp(),
     '/translation': (context) => Translation(),
+    '/key': (context) => MorseKey(),
+    '/send': (context) => Send(),
+    '/inbox': (context) => Inbox(),
   },
 ));
