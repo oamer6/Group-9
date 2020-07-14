b8b6b79bf34d26081aee8c80fbae9db535853e55
ReaganChapman <46576527+ReaganChapman@users.noreply.github.com>
2020-07-14 15:38:38 -0400

    Add files via upload

---
+++ frontend/src/signup.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+
+  void getData() {
+    //API
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        backgroundColor: Colors.grey[200],
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                    child: Text('Sign',
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15.0, 175.0, 0.0, 0.0),
+                    child: Text('Up',
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepOrange))),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepOrange))),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 5.0),
+                    SizedBox(height: 40.0),
+                    Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Colors.deepOrange,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Center(
+                            child: Text(
+                              'Sign Me Up',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  child: Text(
+                    'Changed your mind? Go back to home page.',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
---
+++ frontend/src/translation.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Translation extends StatefulWidget {
+  @override
+  _TranslationState createState() => _TranslationState();
+}
+
+class _TranslationState extends State<Translation> {
+
+  void getData() {
+    //API
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text('My Morse'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 15.0),
+            Row(
+              children: <Widget>[
+                SizedBox(width: 5.0),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/send');
+                  },
+                  child: Text(
+                    'DEBUG ONLY: Go to Send Page',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        decoration: TextDecoration.underline),
+                  ),
+                ),
+              ],
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                hintText: 'Type a message'
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Container(
+              height: 40.0,
+              child: Material(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.deepOrange,
+                elevation: 7.0,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Center(
+                    child: Text(
+                      'Translate',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      )
+    )
+    );
+  }
+}
