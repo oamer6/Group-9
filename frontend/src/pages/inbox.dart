c24cf25fc14ecee7b3ba997ea9f2237898453daf
ReaganChapman <46576527+ReaganChapman@users.noreply.github.com>
2020-07-16 16:57:26 -0400

    Add files via upload
    rough inbox

--- frontend/src/pages/inbox.dart
+++ frontend/src/pages/inbox.dart
@@ -6,14 +6,80 @@ class Inbox extends StatefulWidget {
 }
 
 class _InboxState extends State<Inbox>{
+  int _counter = 0;
+
+  void _incrementCounter(){
+    setState((){
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
         backgroundColor: Colors.deepOrange,
         title: Text('Inbox'),
         centerTitle: true,
         elevation: 0,
+        ),
+      body: Center(
+        child: ListView.builder(
+          itemBuilder: _itemBuilder,
+          itemCount: 5,
+        ),
+      ),
+    );
+  }
+  Widget _itemBuilder(BuildContext context, int index) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Sender $index'),
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: OutlineButton(
+                        onPressed: () => print('English Message'),
+                        // Jared, this needs to display message on press
+                        child: Text('English',
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat'),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                      ),
+                    ),
+                    OutlineButton(
+                      color: Colors.deepOrange,
+                      onPressed: () => print('Morse Message'),
+                      // Jared, this needs to display message on press
+                      child: Text('Morse',
+                        style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat'),
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
