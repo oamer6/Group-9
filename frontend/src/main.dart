c4459cb2854f71c7102631bc218283653f74b70b
ReaganChapman <46576527+ReaganChapman@users.noreply.github.com>
2020-07-18 17:57:27 -0400

    Add files via upload

--- frontend/src/main.dart
+++ frontend/src/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:morseapp/pages/home.dart';
 import 'package:morseapp/pages/translation.dart';
 import 'package:morseapp/pages/loading.dart';
-import 'package:morseapp/pages/login.dart';
 import 'package:morseapp/pages/signup.dart';
 import 'package:morseapp/pages/loading.dart';
 import 'package:morseapp/pages/forgot.dart';
 import 'package:morseapp/pages/inbox.dart';
+import 'package:morseapp/pages/inbox_display.dart';
 import 'package:morseapp/pages/send.dart';
 import 'package:morseapp/pages/key.dart';
 import 'package:morseapp/pages/menu.dart';
+import 'package:morseapp/pages/display.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   initialRoute: '/home',
   routes: {
     '/': (context) => Loading(),
@@ -20,8 +22,10 @@ void main() => runApp(MaterialApp(
     '/forgot': (context) => Forgot(),
     '/signup': (context) => SignUp(),
     '/translation': (context) => Translation(),
+    '/display': (context) => Display(),
     '/key': (context) => MorseKey(),
     '/send': (context) => Send(),
     '/inbox': (context) => Inbox(),
+    '/inbox_display': (context) => InboxDisplay(),
   },
 ));
