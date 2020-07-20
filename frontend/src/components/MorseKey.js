fa6f195c2005bebd8607e912110337ccea3bfc4a
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2020-07-20 14:58:26 -0400

    Make it looks nicer and even

--- frontend/src/components/MorseKey.js
+++ frontend/src/components/MorseKey.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
 
 function MorseKey ()
 {
@@ -9,71 +8,64 @@ function MorseKey ()
                 <h1 className="display-4">International Morse Key</h1>
                 <div class="row">
                     <div class="col-sm-6">
-                       <span className="text-lg font-weight-bold">Letters</span>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">A .-</li>
-                                <li className="list-group-item">B -...</li>
-                                <li className="list-group-item">C -.-.</li>
-                                <li className="list-group-item">D -..</li>
-                                <li className="list-group-item">E .</li>
-                                <li className="list-group-item">F ..-.</li>
-                                <li className="list-group-item">G --.</li>
-                                <li className="list-group-item">H ....</li>
-                                <li className="list-group-item">I ..</li>
-                                <li className="list-group-item">J .---</li>
-                                <li className="list-group-item">K -.-</li>
-                                <li className="list-group-item">L .-..</li>
-                                <li className="list-group-item">M --</li>
-                                <li className="list-group-item">N -.</li>
-                                <li className="list-group-item">O ---</li>
-                                <li className="list-group-item">P .--.</li>
-                                <li className="list-group-item">Q --.-</li>
-                                <li className="list-group-item">R .-.</li>
-                                <li className="list-group-item">S ...</li>
-                                <li className="list-group-item">T -</li>
-                                <li className="list-group-item">U ..-</li>
-                                <li className="list-group-item">V ...-</li>
-                                <li className="list-group-item">W .--</li>
-                                <li className="list-group-item">X -..-</li>
-                                <li className="list-group-item">Y -.--</li>
-                                <li className="list-group-item">Z --..</li>
-                            </ul>
+                       <h4 className="text-lg font-weight-bold">Letters</h4>
+                                <h5 className="text-lg text-monospace">A .-</h5>
+                                <h5 className="text-lg text-monospace">B -...</h5>
+                                <h5 className="text-lg text-monospace">C -.-.</h5>
+                                <h5 className="text-lg text-monospace">D -..</h5>
+                                <h5 className="text-lg text-monospace">E .</h5>
+                                <h5 className="text-lg text-monospace">F ..-.</h5>
+                                <h5 className="text-lg text-monospace">G --.</h5>
+                                <h5 className="text-lg text-monospace">H ....</h5>
+                                <h5 className="text-lg text-monospace">I ..</h5>
+                                <h5 className="text-lg text-monospace">J .---</h5>
+                                <h5 className="text-lg text-monospace">K -.-</h5>
+                                <h5 className="text-lg text-monospace">L .-..</h5>
+                                <h5 className="text-lg text-monospace">M --</h5>
+                                <h5 className="text-lg text-monospace">N -.</h5>
+                                <h5 className="text-lg text-monospace">O ---</h5>
+                                <h5 className="text-lg text-monospace">P .--.</h5>
+                                <h5 className="text-lg text-monospace">Q --.-</h5>
+                                <h5 className="text-lg text-monospace">R .-.</h5>
+                                <h5 className="text-lg text-monospace">S ...</h5>
+                                <h5 className="text-lg text-monospace">T -</h5>
+                                <h5 className="text-lg text-monospace">U ..-</h5>
+                                <h5 className="text-lg text-monospace">V ...-</h5>
+                                <h5 className="text-lg text-monospace">W .--</h5>
+                                <h5 className="text-lg text-monospace">X -..-</h5>
+                                <h5 className="text-lg text-monospace">Y -.--</h5>
+                                <h5 className="text-lg text-monospace">Z --..</h5>
                         </div>
                         <div class="col-sm-6">
-                         <span className="text-lg font-weight-bold">Numbers</span>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">0 -----</li>
-                                <li className="list-group-item">1 .----</li>
-                                <li className="list-group-item">2 ..---</li>
-                                <li className="list-group-item">3 ...--</li>
-                                <li className="list-group-item">4 ....-</li>
-                                <li className="list-group-item">5 .....</li>
-                                <li className="list-group-item">6 -....</li>
-                                <li className="list-group-item">7 --...</li>
-                                <li className="list-group-item">8 ---..</li>
-                                <li className="list-group-item">9 ----.</li>
-                            </ul>
+                         <h4 className="text-lg font-weight-bold">Numbers</h4>
+                                <h5 className="text-lg text-monospace">0 -----</h5>
+                                <h5 className="text-lg text-monospace">1 .----</h5>
+                                <h5 className="text-lg text-monospace">2 ..---</h5>
+                                <h5 className="text-lg text-monospace">3 ...--</h5>
+                                <h5 className="text-lg text-monospace">4 ....-</h5>
+                                <h5 className="text-lg text-monospace">5 .....</h5>
+                                <h5 className="text-lg text-monospace">6 -....</h5>
+                                <h5 className="text-lg text-monospace">7 --...</h5>
+                                <h5 className="text-lg text-monospace">8 ---..</h5>
+                                <h5 className="text-lg text-monospace">9 ----.</h5>
+                         <h4 className="text-lg font-weight-bold">Special character</h4>
+                                <h5 className="text-lg text-monospace">&  .-...</h5>
+                                <h5 className="text-lg text-monospace">'  .----.</h5>
+                                <h5 className="text-lg text-monospace">@  .--.-.</h5>
+                                <h5 className="text-lg text-monospace">)  -.--.-</h5>
+                                <h5 className="text-lg text-monospace">(   -.--.</h5>
+                                <h5 className="text-lg text-monospace">:   ---...</h5>
+                                <h5 className="text-lg text-monospace">,   --..--</h5>
+                                <h5 className="text-lg text-monospace">=  -...-</h5>
+                                <h5 className="text-lg text-monospace">!   -.-.--</h5>
+                                <h5 className="text-lg text-monospace">.   .-.-.-</h5>
+                                <h5 className="text-lg text-monospace">-   -....-</h5>
+                                <h5 className="text-lg text-monospace">+  .-.-.</h5>
+                                <h5 className="text-lg text-monospace">"   .-..-.</h5>
+                                <h5 className="text-lg text-monospace">?  ..--..</h5>    
+                                <h5 className="text-lg text-monospace">/  -..-.</h5> 
                         </div>
-                        <div class="col-sm-6">
-                         <span className="text-lg font-weight-bold">Special character</span>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">&  .-...</li>
-                                <li className="list-group-item">'  .----.</li>
-                                <li className="list-group-item">@  .--.-.</li>
-                                <li className="list-group-item">)  -.--.-</li>
-                                <li className="list-group-item">(   -.--.</li>
-                                <li className="list-group-item">:   ---...</li>
-                                <li className="list-group-item">,   --..--</li>
-                                <li className="list-group-item">=  -...-</li>
-                                <li className="list-group-item">!   -.-.--</li>
-                                <li className="list-group-item">.   .-.-.-</li>
-                                <li className="list-group-item">-   -....-</li>
-                                <li className="list-group-item">+  .-.-.</li>
-                                <li className="list-group-item">"   .-..-.</li>
-                                <li className="list-group-item">?  ..--..</li>    
-                                <li className="list-group-item">/  -..-.</li> 
-                            </ul>
-                        </div>
+                         
                     </div>
                 </div>
             </div>
