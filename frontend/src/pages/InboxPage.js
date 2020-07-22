35eb105fe865c045933e8f794a01eba657a43cae
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2020-07-22 12:11:49 -0400

    add inbox

---
+++ frontend/src/pages/InboxPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Inbox from '../components/Inbox';
+
+const InboxPage = () =>
+{
+    return(
+        <div>
+            <NavBar />
+            <Inbox />
+        </div>
+    );
+};
+
+export default InboxPage;
