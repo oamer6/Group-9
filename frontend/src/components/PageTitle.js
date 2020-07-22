5b03499d32bcb0f0589fb75064a3d58a69d63dfe
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2020-07-22 16:47:29 -0400

    change nav bar

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -3,13 +3,32 @@ import { Link} from 'react-router-dom';
 
 function PageTitle()
 {
+    var ud = localStorage.getItem('user_data');
+    var isLogIn;
+
+    if(ud)
+    {
+        isLogIn = true;
+    }
+    else
+    {
+        isLogIn = false;
+    }
+
+    const doLogout = event =>     
+    {    
+        event.preventDefault();        
+        localStorage.removeItem("user_data");        
+        window.location.href = '/user';    
+    }; 
+
     return(
         <nav className="navbar navbar-expand-lg navbar-warning bg-warning">
             <h1>Morse Code Translator</h1>
             <div className="align-self-end ml-auto align-self-center">
-            <Link to="/morsekey" className="btn btn-outline-alert font-weight-bold">Home</Link>
-            <Link to="/user" className="btn btn-outline-alert font-weight-bold">Login</Link>
-            <Link to="/signup" className="btn btn-outline-alert font-weight-bold">Sign up</Link>
+            <Link to="/morsekey" className="btn btn-outline-alert font-weight-bold">{isLogIn ? 'Morse Code' : 'Home'}</Link>
+            {isLogIn ? <Link to="/inbox" className="btn btn-outline-alert font-weight-bold">Inbox</Link> : <Link to="/user" className="btn btn-outline-alert font-weight-bold">Login</Link>}
+            {isLogIn ? <button type="button" id="logOutButton" className="btn btn-outline-primary font-weight-bold" onClick={doLogout}>Sign Out</button> : <Link to="/signup" className="btn btn-outline-alert font-weight-bold">Sign up</Link>}
             </div>
         </nav> 
     );
