95c1bebf1724b6766e0919954332d7d7a42e888a
Quynh Nguyen <39203622+quynhn1404@users.noreply.github.com>
2020-07-22 23:47:29 -0400

    Not done yet
    first pass on inbox, need to return each message separately

--- frontend/src/components/Inbox.js
+++ frontend/src/components/Inbox.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 const BASE_URL = 'https://mern-morse-code-translator.herokuapp.com';
+
+
 
 function Inbox()
 {
     const [texts, setTexts] = useState('');
+    //var texts;
     var userObj = JSON.parse(localStorage.getItem('user_data'));
     var username = userObj.username;
     const [message,setMessage] = useState('');
-
+    var resultText;
     const loadMessages = async event =>
     {
         event.preventDefault();
@@ -40,16 +43,30 @@ function Inbox()
                 alert(JSON.stringify(res));
                 /*********************************/
                 if (res.results.length > 0) {
-                    setTexts(
-                        // iterate thru results here
-                    );
+                    for(var i = 0; i < res.results.length; i++)
+                    {
+                        resultText += 'From:' + res.results[i].sender + ' , Content: '+ res.results[i].message + '\n';
+                    }
+                    setTexts(resultText);
+                    console.log(texts);
+                    // setTexts(
+                
+                        // // iterate thru results here
+                        // })
+                    // );
                     /*texts = res.map(text => {
                         let sender = text.sender;
                         let content = text.message;
                         let date = text.date;
                         return <span>From: {sender} at {date}. Content: {content}</span>
                     });*/
-                } else {
+                    // texts = res.results.map((text, i) => {
+                    //     return <li key={i}>From :{text.sender}, Content: {text.message}</li>
+                    // })
+ 
+                   // console.log(texts);
+                } 
+                else {
                     setMessage('You have no messages.');
                 }
             }
@@ -61,25 +78,24 @@ function Inbox()
         } 
     };
 
-    return(
-        <div id="inboxDiv" className="container">
-            <script>getMessages</script>
-            <div className="jumbotron">
-                <div className="navbar" id="inboxNavDiv">
-                    <h2 id="inboxHeader">Inbox</h2>
-                    <div className="align-self-end ml-auto">
-                        <Link to="/newmessage" id="newMessageButton">
+        return(
+            <div id="inboxDiv" className="container">
+                <script>getMessages</script>
+                <div className="jumbotron">
+                    <div className="navbar" id="inboxNavDiv">
+                        <h2 id="inboxHeader">Inbox</h2>
+                        <div className="align-self-end ml-auto">
                             <button className="btn btn-outline-primary">+ New Message</button>
-                        </Link>
+                        </div>
                     </div>
+                    <hr />
+                    <button className="btn btn-outline-secondary" onClick={loadMessages}>Load Messages</button>
+                    <p>{texts}</p>  
+                    <p id="inboxResult">{message}</p>
                 </div>
-                <hr />
-                <button className="btn btn-outline-secondary" onClick={loadMessages}>Load Messages</button>
-                <ul>{texts}</ul> 
-                <p id="inboxResult">{message}</p>
             </div>
-        </div>
-    );
+        );
+    
 };
 
 export default Inbox;
